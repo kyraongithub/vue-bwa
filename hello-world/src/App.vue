abdfd24172e82905c081820a4c7acf84d27b67b8
Rizky Ramadhan <71491992+Kyraongithub@users.noreply.github.com>
2021-07-18 13:13:31 +0700

    First time create comp

--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -1,19 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <Logos />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from "./components/HelloWorld.vue";
+import Logos from "./components/Logos.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Logos,
+  },
+};
 </script>
 
 <style>
---
+++ hello-world/src/components/Logos.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h2>Find Best Logo</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  //export untuk dipakai di comp lain
+  name: "logos",
+};
+</script>
+
+<style scoped>
+</style>
